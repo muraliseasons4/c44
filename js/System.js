daf2cced03a427b5b2fc1269e0f66132f5df9ced
Bunny <78802546+muraliseasons4@users.noreply.github.com>
2021-06-21 09:19:50 +0530

    Add files via upload

---
+++ js/System.js
@@ -0,0 +1,27 @@
+class System{
+
+    constructor(){}
+
+    // Add code to authenticate the given code and the access codes.
+    authenticate(actualCode,enteredCode){
+        if(actualCode === enteredCode)
+        return true
+    else
+        false
+    }
+    
+    authenticate(actualCode,enteredCode){
+        if(actualCode === enteredCode)
+        return true
+    else
+        false
+    }
+
+    authenticate(actualCode,enteredCode){
+        if(actualCode === enteredCode)
+        return true
+    else
+        false
+    }
+
+}
